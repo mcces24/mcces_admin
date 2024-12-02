14799171a21b276a31edfdaf79ee8c78eb465b51
mcces24 <158988302+mcces24@users.noreply.github.com>
2024-12-02 11:02:16 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>sAdmin | Madridejos Community College</title>
+    <title>Admin | Madridejos Community College</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
